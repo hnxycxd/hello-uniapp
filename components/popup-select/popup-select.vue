009a47782bce92e100a11362b3cc28e9cf48ce8c
程旭东 <xiyang080@126.com>
2024-09-17 17:59:37 +0800

    init

---
+++ components/popup-select/popup-select.vue
@@ -0,0 +1,124 @@
+<template>
+  <u-popup :show="show" @close="close">
+    <view>
+      <u-search
+        placeholder="请输入关键字搜索"
+        v-model="keyword"
+        custom-style="margin: 20px 10px 5px"
+        @custom="search"
+        @search="search"
+      />
+
+      <scroll-view scroll-y scroll-top="scrollTop" style="max-height: 60vh">
+        <view v-if="multiple">
+          <u-checkbox-group v-model="selectValue" placement="column" @change="radioChange">
+            <u-checkbox
+              :customStyle="{ padding: '10px 8px' }"
+              v-for="item in optionsFilter"
+              :key="item.name"
+              :label="item.label"
+              :name="item.name"
+            ></u-checkbox>
+          </u-checkbox-group>
+        </view>
+
+        <view v-else>
+          <u-radio-group
+            v-model="selectValue"
+            placement="column"
+            borderBottom
+            @change="radioChange"
+          >
+            <u-radio
+              :customStyle="{ padding: '10px 8px' }"
+              v-for="item in optionsFilter"
+              :key="item.name"
+              :label="item.label"
+              :name="item.name"
+            />
+          </u-radio-group>
+        </view>
+      </scroll-view>
+
+      <u-row customStyle="margin-bottom: 10px" gutter="10">
+        <u-col span="6">
+          <u-button @click="cancel">取消</u-button>
+        </u-col>
+        <u-col span="6">
+          <u-button type="primary" @click="confirm">确定</u-button>
+        </u-col>
+      </u-row>
+    </view>
+  </u-popup>
+</template>
+
+<script>
+export default {
+  props: {
+    show: {
+      type: Boolean,
+    },
+    options: {
+      type: Array,
+    },
+    value: {
+      type: String | Number | Array,
+    },
+    multiple: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      selectValue: this.value,
+      keyword: '',
+      optionsFilter: this.options,
+      scrollTop: 0,
+      showOverlay: false,
+    }
+  },
+  watch: {
+    value(val) {
+      this.selectValue = val
+    },
+    options(val) {
+      this.optionsFilter = val
+    },
+  },
+  methods: {
+    search(val) {
+      console.log('search value', val)
+      // this.showOverlay = true;
+      this.$nextTick(() => {
+        this.scrollTop = 0
+        // this.showOverlay = false;
+      })
+
+      if (!val || val.trim() === '') {
+        this.optionsFilter = this.options
+        return
+      }
+
+      this.optionsFilter = this.options.filter(
+        (e) => String(e.name).includes(val) || String(e.label).includes(val)
+      )
+    },
+    radioChange(val) {
+      console.log('radio or checkbox changed', val)
+    },
+    close() {
+      this.$emit('update:show', false)
+    },
+    cancel() {
+      this.close()
+    },
+    confirm() {
+      this.$emit('update:value', this.selectValue)
+      this.close()
+    },
+  },
+}
+</script>
+
+<style></style>
---
+++ pages/index/index.vue
@@ -0,0 +1,96 @@
+<template>
+  <view>
+    <u--form labelPosition="left" :model="formValues" :rules="rules" ref="uForm">
+      <u-form-item label="姓名" prop="name" borderBottom @click="showNameSelect = true">
+        <u--input
+          v-model="formValues.name"
+          border="none"
+          disabled
+          inputAlign="right"
+          class="pop-select"
+        />
+        <u-icon slot="right" name="arrow-right" />
+      </u-form-item>
+
+      <u-form-item label="年龄" prop="age" borderBottom>
+        <u--input v-model="formValues.age" border="none" />
+      </u-form-item>
+
+      <u-form-item label="兴趣" prop="hobby" borderBottom @click="showHobbySelect = true">
+        <u--input v-model="formValues.hobby" border="none" disabled inputAlign="right" />
+        <u-icon slot="right" name="arrow-right" />
+      </u-form-item>
+    </u--form>
+
+    <u-button type="primary" @click="onSubmit">提交</u-button>
+
+    <popup-select
+      :show.sync="showNameSelect"
+      :options="nameOptions"
+      :value.sync="formValues.name"
+    />
+
+    <popup-select
+      multiple
+      :show.sync="showHobbySelect"
+      :options="hobbyOptions"
+      :value.sync="formValues.hobby"
+    />
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      formValues: {
+        name: '7',
+        age: null,
+        hobby: ['3', '5'],
+      },
+      rules: {
+        age: {
+          type: 'number',
+          required: true,
+          message: '请填写年龄',
+          trigger: ['blur', 'change'],
+        },
+      },
+
+      showNameSelect: false,
+      nameOptions: [],
+
+      showHobbySelect: false,
+      hobbyOptions: [],
+    }
+  },
+  onLoad() {},
+  onReady() {
+    this.$refs.uForm.setRules(this.rules)
+    setTimeout(() => {
+      const mockList = Array(300)
+        .fill(null)
+        .map((e, i) => ({
+          label: String(i),
+          name: String(i),
+        }))
+
+      this.nameOptions = mockList
+      this.hobbyOptions = mockList.slice(0, 20)
+    }, 1000)
+  },
+  methods: {
+    onSubmit(values) {
+      this.$refs.uForm.validate().then((res) => {
+        console.log('this.formValue', this.formValues)
+      })
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+.pop-select {
+  background-color: white;
+}
+</style>
